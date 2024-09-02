be7c695eb7f373ed5596784f6488f80c338caf74
alaeddinerami <alaeddinerami7@gmail.com>
2024-09-02 23:18:41 +0100

    Fix:fix css's styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,180 +1,191 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.bordContainer');
-    const turnDisplay = document.getElementById('turn');
-    const playerOne = document.querySelector("#playerOneName");
-    const playerTwo = document.querySelector("#playerTowName"); 
-    const player1Score = document.getElementById('scorePlayerOne');
-    const player2Score = document.getElementById('scorePlayerTow'); 
-    const playersData = JSON.parse(localStorage.getItem("playersScores")) || [];
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".bordContainer");
+  const turnDisplay = document.getElementById("turn");
+  const playerOne = document.querySelector("#playerOneName");
+  const playerTwo = document.querySelector("#playerTowName");
+  const player1Score = document.getElementById("scorePlayerOne");
+  const player2Score = document.getElementById("scorePlayerTow");
+  const playersData = JSON.parse(localStorage.getItem("playersScores")) || [];
 
-    if (playersData.length > 0) {
-      const playerX = playersData[playersData.length - 1].playerOneName;
-      const playerO = playersData[playersData.length - 1].playerTwoName;
+  if (playersData.length > 0) {
+    const playerX = playersData[playersData.length - 1].playerOneName;
+    const playerO = playersData[playersData.length - 1].playerTwoName;
 
-      playerOne.textContent = playerX;
-      playerTwo.textContent = playerO;
+    playerOne.textContent = playerX;
+    playerTwo.textContent = playerO;
 
-      const scores = {
-        [playerX]: playersData[playersData.length - 1].scorePlayerOne || 0,
-        [playerO]: playersData[playersData.length - 1].scorePlayerTwo || 0,
-      };
+    const scores = {
+      [playerX]: playersData[playersData.length - 1].scorePlayerOne || 0,
+      [playerO]: playersData[playersData.length - 1].scorePlayerTwo || 0,
+    };
 
-      player1Score.textContent = scores[playerX];
-      player2Score.textContent = scores[playerO];
+    player1Score.textContent = scores[playerX];
+    player2Score.textContent = scores[playerO];
 
-      let currentPlayer = 'X';
+    let currentPlayer = "X";
 
-      for (let i = 0; i < 400; i++) {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        
-        box.addEventListener('click', () => {
-          if (box.textContent === '') {
-            box.textContent = currentPlayer;
-            
-            const winner = checkWin(); 
-            
-            if (winner) {
-              if (winner === 'X') {
-                scores[playerX]++;
-                player1Score.textContent = scores[playerX];
-                // alert(`${playerX} wins!`);
-                showWinner(playerX);
+    for (let i = 0; i < 400; i++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
 
-              } else {
-                scores[playerO]++;
-                player2Score.textContent = scores[playerO];
-                showWinner(playerO);
-              }
-              
-              playersData[playersData.length - 1].scorePlayerOne = scores[playerX];
-              playersData[playersData.length - 1].scorePlayerTwo = scores[playerO];
-              localStorage.setItem("playersScores", JSON.stringify(playersData));
+      box.addEventListener("click", () => {
+        if (box.textContent === "") {
+          box.textContent = currentPlayer;
 
-              resetGame();
+          const winner = checkWin();
+
+          if (winner) {
+            if (winner === "X") {
+              scores[playerX]++;
+              player1Score.textContent = scores[playerX];
+              // alert(`${playerX} wins!`);
+              showWinner(playerX);
             } else {
-              currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-              updateTurnDisplay();
+              scores[playerO]++;
+              player2Score.textContent = scores[playerO];
+              showWinner(playerO);
             }
-          }
-        });
-        
-        container.appendChild(box);
-      }
 
-      function updateTurnDisplay() {
-        if (currentPlayer === 'X') {
-          turnDisplay.textContent = `${playerX}'s Turn`;
-        } else {
-          turnDisplay.textContent = `${playerO}'s Turn`;
-        }
-      }
-    } else {
-      console.log('No players data found in localStorage');
-    }
+            playersData[playersData.length - 1].scorePlayerOne =
+              scores[playerX];
+            playersData[playersData.length - 1].scorePlayerTwo =
+              scores[playerO];
+            localStorage.setItem("playersScores", JSON.stringify(playersData));
 
-
-    function checkWin() {
-      const boxes = document.querySelectorAll('.box');
-      const board = Array.from(boxes).map(box => box.textContent);
-      const gridSize = 20;
-      const winCondition = 5; 
-    
-      let grid = [];
-      for (let i = 0; i < gridSize; i++) {
-        grid.push(board.slice(i * gridSize, i * gridSize + gridSize));
-      }
-    
-      for (let row = 0; row < gridSize; row++) {
-        for (let col = 0; col < gridSize; col++) {
-          if (grid[row][col] !== '') {
-            if (checkDirection(grid, row, col, 1, 0, winCondition) || 
-                checkDirection(grid, row, col, 0, 1, winCondition) || 
-                checkDirection(grid, row, col, 1, 1, winCondition) || 
-                checkDirection(grid, row, col, 1, -1, winCondition)) { 
-              return grid[row][col]; 
-            }
-          }
-        }
-      }
-      return null; 
-    }
-    
-    function checkDirection(grid, row, col, rowIncrement, colIncrement, winCondition) {
-      const symbol = grid[row][col];
-      let count = 0;
-    
-      for (let i = 0; i < winCondition; i++) {
-        const currentRow = row + i * rowIncrement;
-        const currentCol = col + i * colIncrement;
-    
-        if (currentRow >= 0 && currentRow < grid.length && currentCol >= 0 && currentCol < grid[0].length) {
-          if (grid[currentRow][currentCol] === symbol) {
-            count++;
-            if (count === winCondition) {
-              return true;
-            }
+            resetGame();
           } else {
-            break ; 
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+            updateTurnDisplay();
           }
-        } else {
-          break; 
+        }
+      });
+
+      container.appendChild(box);
+    }
+
+    function updateTurnDisplay() {
+      if (currentPlayer === "X") {
+        turnDisplay.textContent = `${playerX}'s Turn`;
+      } else {
+        turnDisplay.textContent = `${playerO}'s Turn`;
+      }
+    }
+  } else {
+    console.log("No players data found in localStorage");
+  }
+
+  function checkWin() {
+    const boxes = document.querySelectorAll(".box");
+    const board = Array.from(boxes).map((box) => box.textContent);
+    const gridSize = 20;
+    const winCondition = 5;
+
+    let grid = [];
+    for (let i = 0; i < gridSize; i++) {
+      grid.push(board.slice(i * gridSize, i * gridSize + gridSize));
+    }
+
+    for (let row = 0; row < gridSize; row++) {
+      for (let col = 0; col < gridSize; col++) {
+        if (grid[row][col] !== "") {
+          if (
+            checkDirection(grid, row, col, 1, 0, winCondition) ||
+            checkDirection(grid, row, col, 0, 1, winCondition) ||
+            checkDirection(grid, row, col, 1, 1, winCondition) ||
+            checkDirection(grid, row, col, 1, -1, winCondition)
+          ) {
+            return grid[row][col];
+          }
         }
       }
-      return false;
     }
-    
+    return null;
+  }
 
-    
-    function resetGame() {
-      container.querySelectorAll('.box').forEach(box => {
-        box.textContent = '';
-      });
-      currentPlayer = 'X';
-      updateTurnDisplay();
+  function checkDirection(
+    grid,
+    row,
+    col,
+    rowIncrement,
+    colIncrement,
+    winCondition
+  ) {
+    const symbol = grid[row][col];
+    let count = 0;
+
+    for (let i = 0; i < winCondition; i++) {
+      const currentRow = row + i * rowIncrement;
+      const currentCol = col + i * colIncrement;
+
+      if (
+        currentRow >= 0 &&
+        currentRow < grid.length &&
+        currentCol >= 0 &&
+        currentCol < grid[0].length
+      ) {
+        if (grid[currentRow][currentCol] === symbol) {
+          count++;
+          if (count === winCondition) {
+            return true;
+          }
+        } else {
+          break;
+        }
+      } else {
+        break;
+      }
     }
+    return false;
+  }
 
-    
-    document.getElementById('playAgain').addEventListener('click', resetGame);
-    document.getElementById('StartOver').addEventListener('click',e=>{
-      window.location.href = "inputs.html";
-    })
-    
+  function resetGame() {
+    container.querySelectorAll(".box").forEach((box) => {
+      box.textContent = "";
+    });
+    currentPlayer = "X";
     updateTurnDisplay();
+  }
+
+  document.getElementById("playAgain").addEventListener("click", resetGame);
+  document.getElementById("StartOver").addEventListener("click", (e) => {
+    window.location.href = "inputs.html";
   });
 
+  updateTurnDisplay();
+});
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const historyBtn = document.getElementById('history');
-    const historyModal = document.getElementById('historyModal');
-    const closeModal = document.querySelector('.close');
-    const historyList = document.getElementById('historyList');
-  
-    historyBtn.addEventListener('click', () => {
-      displayHistory(); 
-      historyModal.style.display = 'block';
-    });
-  
-    closeModal.addEventListener('click', () => {
-      historyModal.style.display = 'none';
-    });
-  
-    window.addEventListener('click', (event) => {
-      if (event.target === historyModal) {
-        historyModal.style.display = 'none';
-      }
-    });
-  
-    function displayHistory() {
-      const playersData = JSON.parse(localStorage.getItem('playersScores')) || [];
-      historyList.innerHTML = ''; 
-    
-      if (playersData.length === 0) {
-        historyList.innerHTML = '<tr><td colspan="5">No game history available.</td></tr>';
-      } else {
-        playersData.forEach((game, index) => {
-          const row = document.createElement('tr');
-          row.innerHTML = `
+document.addEventListener("DOMContentLoaded", () => {
+  const historyBtn = document.getElementById("history");
+  const historyModal = document.getElementById("historyModal");
+  const closeModal = document.querySelector(".close");
+  const historyList = document.getElementById("historyList");
+
+  historyBtn.addEventListener("click", () => {
+    displayHistory();
+    historyModal.style.display = "block";
+  });
+
+  closeModal.addEventListener("click", () => {
+    historyModal.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === historyModal) {
+      historyModal.style.display = "none";
+    }
+  });
+
+  function displayHistory() {
+    const playersData = JSON.parse(localStorage.getItem("playersScores")) || [];
+    historyList.innerHTML = "";
+
+    if (playersData.length === 0) {
+      historyList.innerHTML =
+        '<tr><td colspan="5">No game history available.</td></tr>';
+    } else {
+      playersData.forEach((game, index) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
             <td>Game ${index + 1}</td>
             <td>${game.playerOneName}</td>
             <td>${game.scorePlayerOne}</td>
@@ -182,26 +193,21 @@
             <td>${game.scorePlayerTwo}</td>
             <td>${game.playerTwoName}</td>
           `;
-          historyList.appendChild(row);
-        });
-      }
+        historyList.appendChild(row);
+      });
     }
-  });
-
-
-
-  const winnerDisplay = document.getElementById('winnerDisplay');
-  const winnerText = document.getElementById('winnerText');
-  const closeWinnerDisplay = document.querySelector('#winnerDisplay .close');
-
-
-  function showWinner(winnerName) {
-    winnerText.textContent = `${winnerName} wins!`;
-    winnerDisplay.classList.remove('hidden');
   }
-  closeWinnerDisplay.addEventListener('click', () => {
-    winnerDisplay.classList.add('hidden');
-  });
+});
 
+const winnerDisplay = document.getElementById("winnerDisplay");
+const winnerText = document.getElementById("winnerText");
+const closeWinnerDisplay = document.querySelector("#winnerDisplay .close");
 
-  
+function showWinner(winnerName) {
+  winnerText.textContent = `${winnerName} wins!`;
+  winnerDisplay.classList.remove("hidden");
+}
+closeWinnerDisplay.addEventListener("click", () => {
+  winnerDisplay.classList.add("hidden");
+});
+
